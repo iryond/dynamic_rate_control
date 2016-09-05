1c487577b62c78866565e78b8b46c14038cbcde1
Ryo.Ishimoto <iryond@gmail.com>
2016-09-05 23:57:44 +0900

    First Commit.

---
+++ rate_control.py
@@ -0,0 +1,69 @@
+import boto3
+import sys
+import json,urllib2
+
+#### for test input
+json_text='{"InstanceStates": [{"InstanceId": "i-xxxxxxxx","ReasonCode": "N/A","State": "InService","Description": "N/A"},{"InstanceId": "i-xxxxxxxx","ReasonCode": "N/A","State": "InService","Description": "N/A"}]}'
+
+#print (root)
+#print (root['InstanceStates'])
+
+#### pre
+TARGET_STATE = "InService"
+
+#### main
+ssm = boto3.client('ssm')
+ec2 = boto3.client('ec2')
+InstanceStates = json.loads(json_text)
+Instances = InstanceStates['InstanceStates']
+count = len(Instances)
+INSERVICE_NUM = 0
+
+for n in range(0,count):
+  InstanceState = Instances[n]['State']
+  if InstanceState == TARGET_STATE:
+    INSERVICE_NUM = INSERVICE_NUM + 1
+
+print(INSERVICE_NUM)
+
+##### Exex SSM
+
+responce = ec2.describe_instances(
+    DryRun=True,
+    Filters=[
+        {
+            'Name': 'tag',
+            'Values': [
+                'xxxxx*',
+            ]
+        },
+    ],
+    NextToken='string',
+    MaxResults=123
+)
+
+response = ssm.send_command(
+    InstanceIds=[
+        'string',
+    ],
+    DocumentName='string',
+    DocumentHash='string',
+    DocumentHashType='Sha256'|'Sha1',
+    TimeoutSeconds=123,
+    Comment='string',
+    Parameters={
+        'string': [
+            'string',
+        ]
+    },
+    OutputS3BucketName='string',
+    OutputS3KeyPrefix='string',
+    ServiceRoleArn='string',
+    NotificationConfig={
+        'NotificationArn': 'string',
+        'NotificationEvents': [
+            'All'|'InProgress'|'Success'|'TimedOut'|'Cancelled'|'Failed',
+        ],
+        'NotificationType': 'Command'|'Invocation'
+    }
+)
