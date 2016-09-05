45de8573bbdf6a2beeb7cb730b4e199be6f1ca63
iryond <ishimotoru@nttdata.co.jp>
2016-09-05 21:59:57 +0900

    Update rate_control.py

--- rate_control.py
+++ rate_control.py
@@ -3,7 +3,7 @@ import sys
 import json,urllib2
 
 #### for test input
-json_text='{"InstanceStates": [{"InstanceId": "i-1add5d1f","ReasonCode": "N/A","State": "InService","Description": "N/A"},{"InstanceId": "i-b9bb39be","ReasonCode": "N/A","State": "InService","Description": "N/A"}]}'
+json_text='{"InstanceStates": [{"InstanceId": "i-xxxxxxxx","ReasonCode": "N/A","State": "InService","Description": "N/A"},{"InstanceId": "i-xxxxxxxx","ReasonCode": "N/A","State": "InService","Description": "N/A"}]}'
 
 #print (root)
 #print (root['InstanceStates'])
